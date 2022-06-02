2904a26d14e376e336acff45cc99436fb5ebf57f
EliteForce1 <xanderveninga@gmail.com>
2022-06-02 12:54:50 +0200

    Working shotgun + script to damage enemy's
    reload
    when shooting ammo goes down

---
+++ Assets/Xander/Assets/Scripts/ScriptableObjects/Gun.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] GunData gunData;
+    [SerializeField] Transform muzzle;
+
+
+    float timeSinceLastShot;
+
+    public void Awake()
+    {
+        gunData.reloading = false;
+    }
+    public void Start()
+    {
+        PlayerShoot.shootInput += Shoot;
+        PlayerShoot.reloadInput += StartReload;
+    }
+
+    public void StartReload()
+    {
+        if(!gunData.reloading)
+        {
+            StartCoroutine(Reload());
+        }
+    }
+
+    private IEnumerator Reload()
+    {
+        gunData.reloading = true;
+
+        yield return new WaitForSeconds(gunData.reloadTime);
+
+        gunData.currentAmmo = gunData.magSize;
+
+        gunData.reloading = false;
+    }
+
+    private bool CanShoot() => !gunData.reloading && timeSinceLastShot > 1f / (gunData.fireRate / 60f);
+    public void Shoot()
+    {
+        if(gunData.currentAmmo > 0)
+        {
+            if(CanShoot())
+            {
+                if(Physics.Raycast(muzzle.position, muzzle.forward, out RaycastHit hitInfo, gunData.maxDistance))
+                {
+                    IDamageable damageable = hitInfo.transform.GetComponent<IDamageable>();
+                    damageable?.TakeDamage(gunData.damage);
+                }
+                gunData.currentAmmo--;
+                timeSinceLastShot = 0;
+                OnGunShot();
+            }
+        }
+    }
+
+    private void Update()
+    {
+        timeSinceLastShot += Time.deltaTime;
+    }
+    private void OnGunShot()
+    {
+        
+    }
+}
