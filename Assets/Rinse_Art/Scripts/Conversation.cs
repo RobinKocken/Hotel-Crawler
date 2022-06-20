e75919c45fbe2158a94ae4ca442b06e7575a6148
Rinse Wernik <rinse.wernik@outlook.com>
2022-06-20 12:52:45 +0200

    Dragon Fly

---
+++ Assets/Rinse_Art/Scripts/Conversation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conversation : MonoBehaviour
+{
+    public string[] questions; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
