75fe2c880c49f92ef9073424afcf042abf800810
EliteForce1 <xanderveninga@gmail.com>
2022-06-30 15:37:25 +0200

    Update FPSCamera.cs

--- Assets/Robin/Scripts/Player/FPSCamera.cs
+++ Assets/Robin/Scripts/Player/FPSCamera.cs
@@ -72,11 +72,11 @@ public class FPSCamera : MonoBehaviour
         Debug.DrawRay(transform.position, transform.forward * rayDistance, Color.red);
         if(Physics.Raycast(transform.position, transform.forward, out hit, rayDistance, doorLayer))
         {
-            if(Input.GetButtonDown("E") && hit.transform.tag != "Scene")
+            if(Input.GetKeyDown(interactKey) && hit.transform.tag != "Scene")
             {
                 hit.transform.GetComponent<DoorScript>().DoorActivate();
             }
-            else if(Input.GetButtonDown("E") && hit.transform.tag == "Scene")
+            else if(Input.GetKeyDown(interactKey) && hit.transform.tag == "Scene")
             {
                 hit.transform.GetComponent<SceneScript>().LoadLevelOne();
             }
