3299f61cb31628d60eb10a354fd1e40d46dfb093
Xander Veninga <xanderveninga@gmail.com>
2022-06-30 19:17:40 +0200

    small UI update

---
+++ Assets/DisplayInventory.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayInventory : MonoBehaviour
+{
+    public InventoryObject inventory;
+
+    public int X_SPACE_BETWEEN_ITEM;
+    public int NUMBER_OF_COLUMNS;
+    public int Y_SPACE_BETWEEN_ITEM;
+    Dictionary<InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventorySlot, GameObject> ();
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateDisplay();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //UpdateDisplay();
+    }
+
+    public void CreateDisplay()
+    {
+        for(int i = 0; i < inventory.Container.Count; i++)
+        {
+            var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+            obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+            obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+        }
+    }
+
+    public Vector3 GetPosition(int i)
+    {
+        return new Vector3((X_SPACE_BETWEEN_ITEM * (i % NUMBER_OF_COLUMNS)), (-Y_SPACE_BETWEEN_ITEM * (i / NUMBER_OF_COLUMNS)), 0f);
+    }
+}
